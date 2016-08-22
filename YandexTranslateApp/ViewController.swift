1ac671f9c3ca57049e30b9fdfa4ac8b7ecb478a5
Anna Gorobchenko <annagorobchenko@mac-mini-anna.local>
2016-08-22 09:54:30 +0300

    Initial Commit

---
+++ YandexTranslateApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YandexTranslateApp
+//
+//  Created by Anna Gorobchenko on 22.08.16.
+//  Copyright © 2016 Anna Gorobchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
