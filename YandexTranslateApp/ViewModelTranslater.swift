b7defa270855c37725b4d0100a66adfefdd8e3fc
Anna Gorobchenko <annagorobchenko@mac-mini-anna.local>
2016-08-23 18:09:51 +0300

    added guard instead of if - else conditions

--- YandexTranslateApp/ViewModelTranslater.swift
+++ YandexTranslateApp/ViewModelTranslater.swift
@@ -23,55 +23,67 @@ class ViewModelTranslater {
         let current = inputText
         isAnimating = true
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(1/2 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
             
-            if inputText.characters.count > 2 {
-                
-                if self.previous == inputText {
-                    
-                    self.yandexServiceApi.sendRequestText (inputText) { [weak self] data in
-                            print("send request \(inputText)")
-                            let translationListModel = data.1
-                            
-                            if  let value = translationListModel.first {
-                                
-                                self!.translatedText = (value.tr?.first?.text)!
-                                self!.isAnimating = false
-                                
-                            }
-                        
-                        self!.notify()
-                    }
-                    
-                } else {
-                    
-                    self.translatedText = "Error, smth wrong"
-                    self.isAnimating = false
-                    
-                }
-                
-            } else if inputText.characters.count == 0 {
+          
+            if inputText.characters.count == 0 {
                 
                 self.isAnimating = false
                 self.translatedText = ""
-                
-            } else {
-                
-                self.translatedText = "Error, too short word"
-                self.isAnimating = false
+                self.notify()
+                return
                 
             }
             
-            self.notify()
+            guard inputText.characters.count > 2 else {
+            
+                self.translatedText = "Error, too short word"
+                self.isAnimating = false
+                self.notify()
+                return
+            
+            }
+            
+            guard self.previous == inputText else {
+                
+                self.translatedText = "Error, smth wrong"
+                self.isAnimating = false
+                return
+                
+            }
+            
+            
+            self.yandexServiceApi.sendRequestText (inputText) { [weak self] data in
+                print("send request \(inputText)")
+                let translationListModel = data.1
+                
+                if  let value = translationListModel.first {
+                    
+                    self!.translatedText = (value.tr?.first?.text)!
+                    self!.isAnimating = false
+                    
+                } else {
+                    print("yandexServiceApi Server error, check request")
+                    self!.translatedText = "Server error, check request"
+                    self!.isAnimating = false
+                    
+                }
+
+                
+            self!.notify()
+                
+            }
+        
         });
         
         let temp = current
         self.previous = temp
         
         notify()
+       
     }
     
-    
+
     private func notify() {
         
         self.callback?(translatedText, isAnimating)
