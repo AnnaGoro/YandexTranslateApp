f841e98ca49fbe507971215d60a343d896e749cf
Anna Gorobchenko <annagorobchenko@mac-mini-anna.local>
2016-08-26 14:34:28 +0300

    edited ViewController and ViewModel, added Observable var

--- YandexTranslateApp/ViewController.swift
+++ YandexTranslateApp/ViewController.swift
@@ -20,49 +20,8 @@ class ViewController: UIViewController, UITextFieldDelegate  {
         actInd.activityIndicatorViewStyle  = UIActivityIndicatorViewStyle.Gray;
         actInd.center = view.center
         
-        inputTextToTranslate.rx_text
-            
-            .throttle(0.4, scheduler: MainScheduler.instance) // Wait 0.4 for changes.
-            
-            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old value
-            
-            .filter ({ (text : String?) -> Bool in
-                let checkCount = text!.characters.count == 0
-                
-                if checkCount {
-                    
-                    self.viewModelTranslater.isAnimating.value = false
-                    self.viewModelTranslater.translatedText.value  = ""
-                    
-                    return !checkCount
-                    
-                }
-                return !checkCount
-            })
-            
-            .filter ({ (text : String?) -> Bool in
-                
-                let checkCount = text!.characters.count < 3
-                if checkCount{
-                    
-                    self.viewModelTranslater.isAnimating.value = false
-                    self.viewModelTranslater.translatedText.value = "Oops, too short word"
-                    
-                    return !checkCount
-                    
-                }
-                return !checkCount
-                }
-            )
-            
-            .subscribeNext { [unowned self] (textToTranslate : String) -> Void in
-                
-                self.viewModelTranslater.changedText(textToTranslate)
-                
-            }
-            .addDisposableTo(bag)
-        
-        
+        viewModelTranslater.observableViewModelTextField = inputTextToTranslate.rx_text.asObservable()
+              
         viewModelTranslater.translatedText.asObservable()
             .subscribeNext { (translatedText: String) -> Void in
                 self.translatedText.text = translatedText
--- YandexTranslateApp/ViewModelTranslater.swift
+++ YandexTranslateApp/ViewModelTranslater.swift
@@ -1,5 +1,4 @@
 
-
 import Foundation
 import UIKit
 import RxSwift
@@ -7,11 +6,58 @@ import RxSwift
 struct ViewModelTranslater {
     
     var translatedText: Variable <String> = Variable( "" )
-    
-    private var yandexServiceApi = YandexServiceApi()
     var isAnimating : Variable <Bool> = Variable( false )
     
+    private var yandexServiceApi = YandexServiceApi()
     private let bag = DisposeBag()
+    
+    var observableViewModelTextField : Observable <String>? {
+        
+        didSet {
+            guard let observable = observableViewModelTextField else { return }
+            
+            observable
+                
+                .throttle(0.4, scheduler: MainScheduler.instance) // Wait 0.4 for changes.
+                
+                .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old value
+                
+                .filter ({ (text : String?) -> Bool in
+                    let checkCount = text!.characters.count == 0
+                    
+                    if checkCount {
+                        
+                        self.isAnimating.value = false
+                        self.translatedText.value  = ""
+                        
+                        return !checkCount                        
+                    }
+                    return !checkCount
+                })
+                
+                .filter ({ (text : String?) -> Bool in
+                    
+                    let checkCount = text!.characters.count < 3
+                    if checkCount{
+                        
+                        self.isAnimating.value = false
+                        self.translatedText.value = "Oops, too short word"
+                        
+                        return !checkCount
+                    }
+                    return !checkCount
+                    }
+                )
+                
+                .subscribeNext { (inputText: String) -> Void in
+                    self.changedText(inputText)
+                }
+                .addDisposableTo(bag)
+            
+        }
+        
+    }
+    
     
     mutating func changedText(text : String) {
         
@@ -24,9 +70,8 @@ struct ViewModelTranslater {
                     
                     self.translatedText.value = translatedTextYandex!
                     self.isAnimating.value = false
-                                       
+                    
                 },
-                
                 onError: { translatedTextYandex in
                     
                     self.translatedText.value = "Error, check your network connection"
